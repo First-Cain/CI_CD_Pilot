15500d6b851252db4353f04f1691a0af45ac7e52
AdamDeee <113461389+AdamDeee@users.noreply.github.com>
2026-02-02 00:23:20 +0300

    Добавьте файлы проекта.

---
+++ CI_CD_Pilot.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+
+
+
+
+
+
+
+
+
+// Тестовый класс для CI/CD пайплайна
+class TestClass {
+    public:
+	static void display_message() {
+        std::cout << "This is a test class for CI/CD pipeline.\n";
+    }
+};
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+    TestClass::display_message();
+	return 0;
+}
