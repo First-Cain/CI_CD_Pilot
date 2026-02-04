a1559f0dab4854cfbeae9a69e17a46f64b8d8cf5
AdamDeee <113461389+AdamDeee@users.noreply.github.com>
2026-02-05 01:09:00 +0300

    Add CP

--- CI_CD_Pilot.cpp
+++ CI_CD_Pilot.cpp
@@ -1,20 +1,23 @@
 ﻿#include <iostream>
-
-
-
-
-
-
+#include <version>
 
 
 
 
 // Тестовый класс для CI/CD пайплайна
 class TestClass {
-    public:
+
+	int a_{ 15 };
+
+	public:
 	static void display_message() {
-        std::cout << "This is a test class for CI/CD pipeline.\n";
-    }
+		std::cout << "This is a test class for CI/CD pipeline.\n";
+	}
+
+	void new_test_foo() const
+	{
+		std::cout << "test " << a_ << "\n";
+	}
 };
 
 
@@ -27,7 +30,48 @@ class TestClass {
 
 int main()
 {
+	TestClass::display_message();
+	TestClass obj;
 
-    TestClass::display_message();
+	obj.new_test_foo();
+
+	constexpr int a{ 2 };
+
+
+
+
+	std::cout << "	[" << a << "]	 WTF BRED\n\n\n";
+
+	std::cout << "	[" << a << "]	Hello CMake.\n\n";
+
+
+
+
+	// Выводим тип активной платформы
+#ifdef WIN32
+	std::cout << "	[" << a << "]	Windows\n";
+#elif defined(__linux__)
+	std::cout << "	[" << a << "]	Linux\n";
+#elif defined(__APPLE__) && defined(__MACH__)
+	std::cout << "	[" << a << "]	macOS\n";
+#else
+	std::cout << "	[" << a << "]	Unknown platform\n";
+#endif
+
+
+
+	// Выводим версию и имя компилятора и стандарт языка
+#ifdef __clang__
+	std::cout << "	[" << a << "]	Clang " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__ << "\n";
+#elif defined(__GNUC__)
+	std::cout << "	[" << a << "]	GCC " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << "\n";
+#elif defined(_MSC_VER)
+	std::cout << "	[" << a << "]	MSVC " << _MSC_VER << "\n";
+#else
+	std::cout << "	[" << a << "]	Unknown compiler\n";
+#endif
+
+
+
 	return 0;
 }
---
+++ build/windows-msvc/CTestTestfile.cmake
@@ -0,0 +1,21 @@
+# CMake generated Testfile for 
+# Source directory: D:/Hard_Code/VS Projects/CI_CD_Pilot
+# Build directory: D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test([=[BasicTest]=] "D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/Debug/CI_CD_Pilot.exe")
+  set_tests_properties([=[BasicTest]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;82;add_test;D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test([=[BasicTest]=] "D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/Release/CI_CD_Pilot.exe")
+  set_tests_properties([=[BasicTest]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;82;add_test;D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test([=[BasicTest]=] "D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/MinSizeRel/CI_CD_Pilot.exe")
+  set_tests_properties([=[BasicTest]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;82;add_test;D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test([=[BasicTest]=] "D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/RelWithDebInfo/CI_CD_Pilot.exe")
+  set_tests_properties([=[BasicTest]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;82;add_test;D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;0;")
+else()
+  add_test([=[BasicTest]=] NOT_AVAILABLE)
+endif()
