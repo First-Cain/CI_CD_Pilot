48b879af8bc1b59396b470881d0474f58b1aada5
First-Cain <First-Cain@yandex.ru>
2026-02-08 18:02:32 +0300

    Delete build directory

--- build/windows-clang/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Hard_Code/VS Projects/CI_CD_Pilot
-# Build directory: D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-clang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[BasicTest]=] "D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-clang/CI_CD_Pilot.exe")
-set_tests_properties([=[BasicTest]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;82;add_test;D:/Hard_Code/VS Projects/CI_CD_Pilot/CMakeLists.txt;0;")
--- build/windows-msvc/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/_deps/googletest-src
-# Build directory: D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
