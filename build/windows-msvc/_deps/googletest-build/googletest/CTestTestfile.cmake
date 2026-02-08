b4501e501a951404d941eb1f6630ee71544c7245
AdamDeee <113461389+AdamDeee@users.noreply.github.com>
2026-02-08 18:23:30 +0300

    Ignore build folder

--- build/windows-msvc/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/_deps/googletest-src/googletest
-# Build directory: D:/Hard_Code/VS Projects/CI_CD_Pilot/build/windows-msvc/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
